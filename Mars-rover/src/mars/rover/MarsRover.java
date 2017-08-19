9795f9132b475413a5fa84a1a382fdb80185fdc7
Christopher da Rosa Michel <christopher.michel@cwi.com.br>
2017-08-19 14:06:18 -0300

    feat(projeto): criar projeto base.

---
+++ Mars-rover/src/mars/rover/MarsRover.java
@@ -0,0 +1,15 @@
+package mars.rover;
+
+/**
+ *
+ * @author chris
+ */
+public class MarsRover {
+    
+    //classe principal para a executação do programa
+    
+    public static void main(String[] args) {
+
+    }
+    
+}
